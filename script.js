453e59c87de5b6adc913242ccf23fafef8522185
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-10-02 14:07:51 -0300

    Fazendo chamada API.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.querySelector(".busca").addEventListener("submit", (event) => {
   event.preventDefault();
+
+  let input = document.querySelector("#searchInput").value;
+
+  if (input !== "") {
+    showWarning("Carregando...");
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
+      input
+    )}&appid=270203d9cab5cb5be2c18ed3f7e91262`;
+  }
 });
+
+function showWarning(msg) {
+  document.querySelector(".aviso").innerHTML = msg;
+}
